4c2937e9e5e1261e0d658e9146a6d94a100cb2e1
Bernkastel <valuableodai@gmail.com>
2023-10-29 23:34:43 +0300

    Update 2.c

--- Chapter 4/2.c
+++ Chapter 4/2.c
@@ -6,7 +6,9 @@ int main(void)
   int num;
   printf("Enter a three-digit number: ");
   scanf("%d", &num);
-
+  
+  printf("The reversal is: ");
+  
   int first = num % 10;
   printf("%d", first);
 
