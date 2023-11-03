003d6e0c5d6ea5a8ef208382bf6e6c03f7301ef8
Bernkastel <valuableodai@gmail.com>
2023-11-03 20:12:21 +0300

    Update 6.c

--- Chapter 5/6.c
+++ Chapter 5/6.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12;
-    printf("Enter the UPC code: ");
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
-          &i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11, &i12);
+  int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12;
+  printf("Enter the UPC code: ");
+  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+        &i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11, &i12);
 
   int first_sum = i1 + i3 + i5 + i7 + i9 + i11;
   int second_sum = i2 + i4 + i6 + i8 + i10;
