27d60675aaa3e359cc803bb4cb658b1d8b95408f
Bernkastel <valuableodai@gmail.com>
2023-12-02 17:48:54 +0300

    Update 02.c

--- Chapter 7/02.c
+++ Chapter 7/02.c
@@ -6,15 +6,16 @@ int main(void)
   printf("This program prints a table of squares.\n");
   printf("Enter number of entries in table: ");
   scanf("%d", &n);
-  getchar(); // to consume the \n character after the user hits enter, since scanf keeps it in the buffer
+  getchar(); // to consume the \n character after the input
 
   for (i = 1; i <= n; i++)
   {
     printf("%10d%10d\n", i, i * i);
-    if ( i % 24 == 0)
+    if (i % 24 == 0)
     {
       printf("Press Enter to continue...");
-      getchar();
+      while (getchar() != '\n')
+        continue;
     }
   }
 
