466f008415f7c92e56f756bd75b404e37352e7a0
Bernkastel <valuableodai@gmail.com>
2023-12-10 22:47:59 +0300

    Create 14.c

---
+++ Chapter 7/14.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+  printf("Enter a positive number: ");
+  float x;
+  scanf("%f", &x);
+
+  float y = 1, div, avg, prev_y;
+  do
+  {
+      div = x / y;
+      avg = (y + div) / 2;
+      prev_y = y;
+      y = avg;
+  } while (fabs(prev_y - y) > 0.00001);
+  
+  printf("Square root: %f", y);
+
+  return 0;
+}
